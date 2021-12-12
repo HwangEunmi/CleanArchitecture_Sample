917330da17e22e4d6221edfd146ee81f675f2771
cncn6666 <cncn6666@naver.com>
2021-12-12 14:46:30 +0900

    [ADD] RepoInfo domain 모델 클래스 추가

---
+++ app/src/main/java/com/kakaovx/practice/cleanarchitecturepractice/domain/model/repo/RepoInfoDataModel.kt
@@ -0,0 +1,19 @@
+package com.kakaovx.practice.cleanarchitecturepractice.domain.model.repo
+
+data class RepoInfoDataModel(
+    val repoId: Int,
+    val name: String,
+    val fullName: String,
+    val owner: Owner
+) {
+    // 해당 View에 필요한 변수 선언
+    var repoNum: Int = 0
+
+    data class Owner(
+        val profileImgUrl: String,
+        val isAdmin: Boolean
+    )
+
+    // 확장함수에 필요함
+    companion object
+}
