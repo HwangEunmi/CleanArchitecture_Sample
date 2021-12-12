9302f12963ae6cff81e743c3cb77d97b236f149f
cncn6666 <cncn6666@naver.com>
2021-12-12 14:46:35 +0900

    [ADD] UserInfo domain 모델 클래스 추가

---
+++ app/src/main/java/com/kakaovx/practice/cleanarchitecturepractice/domain/model/user/UserInfoDataModel.kt
@@ -0,0 +1,7 @@
+package com.kakaovx.practice.cleanarchitecturepractice.domain.model.user
+
+data class UserInfoDataModel(
+    val userId: Int,
+    val userName: String,
+    val profileImgUrl: String
+)
