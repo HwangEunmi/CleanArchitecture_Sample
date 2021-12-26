6851b5a0d41b0776e59ef64859f6f4ff8c8b8622
cncn6666 <cncn6666@naver.com>
2021-12-26 17:19:01 +0900

    [UPDATE] launcher용 MainActivity 생성

---
+++ app/src/main/java/com/kakaovx/practice/cleanarchitecturepractice/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kakaovx.practice.cleanarchitecturepractice.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kakaovx.practice.cleanarchitecturepractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
