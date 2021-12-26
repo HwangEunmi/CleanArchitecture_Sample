a3230ad5624e07b17aea8a9cd452803efc10509c
cncn6666 <cncn6666@naver.com>
2021-12-26 17:28:37 +0900

    [ADD] 버튼 클릭시 DetailActivity로 이동하는 로직 추가

--- app/src/main/java/com/kakaovx/practice/cleanarchitecturepractice/ui/MainActivity.kt
+++ app/src/main/java/com/kakaovx/practice/cleanarchitecturepractice/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.kakaovx.practice.cleanarchitecturepractice.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kakaovx.practice.cleanarchitecturepractice.R
+import com.kakaovx.practice.cleanarchitecturepractice.databinding.ActivityMainBinding
+import com.kakaovx.practice.presentation.ui.DetailActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.btnMoveDetail.setOnClickListener {
+            startActivity(Intent(this, DetailActivity::class.java))
+        }
     }
 }
